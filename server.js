0766a07f5b4e1c5f8a1eb0ebd15eaa5b502944ef
yoshi-app <admin@aiyoshiapp.com>
2026-05-28 06:09:43 +0000

    feat: initial commit for lambda version

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const handler = require('./api/generate');
+app.post('/api/generate',(req, res) => handler(req, res));
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+} );
+
